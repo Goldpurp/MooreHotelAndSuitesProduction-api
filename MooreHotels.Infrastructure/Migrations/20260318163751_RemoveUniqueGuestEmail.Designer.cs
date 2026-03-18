64f4db1bb53321fcf08b6166bdfecf8bc3d587fb
Kehinde Razaq <Lilswatch112@gmail.com>
2026-03-18 17:42:17 +0100

    update migration

--- MooreHotels.Infrastructure/Migrations/20260318163751_RemoveUniqueGuestEmail.Designer.cs
+++ MooreHotels.Infrastructure/Migrations/20260318163751_RemoveUniqueGuestEmail.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MooreHotels.Infrastructure.Persistence;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MooreHotels.Infrastructure.Migrations
 {
     [DbContext(typeof(MooreHotelsDbContext))]
-    partial class MooreHotelsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260318163751_RemoveUniqueGuestEmail")]
+    partial class RemoveUniqueGuestEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
